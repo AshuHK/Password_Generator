1a919e87e83017cb4f9134406d1681ad29a2f39d
Ashu Kher <ashukher1@gmail.com>
2020-05-27 09:34:04 -0400

    Update gui_generator.py

--- gui_generator.py
+++ gui_generator.py
@@ -12,8 +12,17 @@ root.configure(bg="white")
 Label(root, text="Length of password: ", bg="white").grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
 # entry for the password size
+size_entry = Entry(root) 
+size_entry.grid(row=0, column=1, padx=5, pady=5, sticky="w")
 
+# generate button 
 generate_button = Button(text="Generate", font=("Arial"), bg="red", fg="black", highlightbackground="red")
 generate_button.grid(row=1, column=1, padx=5, pady=5)
+
+Label(root, text="Password: ", bg="white").grid(row=2, column=0, padx=5, pady=5)
+
+# where the password goes 
+password_dest = Entry(root)
+password_dest.grid(row=2, column=1, padx=5, pady=5)
 
 root.mainloop()
